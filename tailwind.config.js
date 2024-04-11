ee72266494dbca8cd5b86c65b022409735a97a4a
TotallyNotIvan <143670358+TotallyNotIvan@users.noreply.github.com>
2024-04-10 20:27:03 -0400

    tried to fallow tutorial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [], 
+  theme: {
+    extend: {},
+    container: {
+      // center: true,
+      // padding: '2rem',
+    }
+  },
+  plugins: [],
+}
+
